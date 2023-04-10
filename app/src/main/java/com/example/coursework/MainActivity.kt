61842dd363f6a08d92b5487677b69f54d5568c0f
UndetectedMax <maxm13052004@gmail.com>
2023-04-10 19:47:16 +0300

    hghg

--- app/src/main/java/com/example/coursework/MainActivity.kt
+++ app/src/main/java/com/example/coursework/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.coursework.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
