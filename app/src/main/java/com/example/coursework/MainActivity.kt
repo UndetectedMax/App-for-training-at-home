17945f42138ca111b68e53f3ad14ad13169f1377
UndetectedMax <maxm13052004@gmail.com>
2023-04-10 21:53:52 +0300

    hghg

--- app/src/main/java/com/example/coursework/MainActivity.kt
+++ app/src/main/java/com/example/coursework/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.coursework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
 import com.example.coursework.databinding.ActivityMainBinding
 
 
@@ -10,5 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemReselectedListener {
+            when(it.itemId) {
+                   R.id.train_icon -> switchFragment("Train icon")
+                   R.id.add_training -> switchFragment("Add training icon")
+                   R.id.statistic -> switchFragment("Statistic icon")
+                   R.id.settings_icon -> switchFragment("Settings icon")
+            }
+        }
+        binding.bottomNavigationView.selectedItemId = R.id.train_icon
+    }
+
+    private fun switchFragment(icon:String) {
+        supportFragmentManager.commit {
+            replace(R.id.fragmentContainerView, FirstFragment.newInstance(icon))
+        }
     }
 }
+
+
+
+
