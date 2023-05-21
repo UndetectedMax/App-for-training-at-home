14f54b420202a5148377ee1ce077dfd60828f753
UndetectedMax <maxm13052004@gmail.com>
2023-05-21 22:32:00 +0300

    webView in Activity(made some not big changes)

--- app/src/main/java/com/example/coursework/screens/trainings/TrainDetails.kt
+++ app/src/main/java/com/example/coursework/screens/trainings/TrainDetails.kt
@@ -1,7 +1,6 @@
 package com.example.coursework.screens.trainings
 
 import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.webkit.WebResourceError
@@ -20,25 +19,18 @@ class TrainDetails : AppCompatActivity() {
 
     private lateinit var webView: WebView
 
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-    }
-
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = TrainDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         webView = binding.webview.findViewById(R.id.webview)
-        webView.settings.builtInZoomControls = true
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.settings.defaultTextEncodingName = "UTF-8"
         webView.webViewClient = MyWebViewClient()
         webView.clearCache(true)
         WebView.setWebContentsDebuggingEnabled(true)
-        webView.loadUrl("file:///android_asset/files/index.html")
         val title = intent.getStringExtra("text").toString()
         when (title) {
             "Cardio" -> webView.loadUrl("file:///android_asset/files/cardio_trainings.html")
@@ -67,11 +59,8 @@ class TrainDetails : AppCompatActivity() {
             error: WebResourceError?
         ) {
             super.onReceivedError(view, request, error)
-            val errorMessage = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val errorMessage =
                 error?.description?.toString()
-            } else {
-                "Unknown error"
-            }
             Log.e("WebViewError", "Error: $errorMessage")
         }
 
