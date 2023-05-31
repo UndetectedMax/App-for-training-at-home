256cf67dfaff85929f85d396cc56484db1a5899d
VitaliyS <vitaliys13.11@gmail.com>
2023-05-31 17:53:24 +0300

    Scrolling in TextView

--- app/src/main/java/com/example/coursework/screens/adding/AddedTrainDetails.kt
+++ app/src/main/java/com/example/coursework/screens/adding/AddedTrainDetails.kt
@@ -12,6 +12,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -80,7 +81,9 @@ class AddedTrainDetails : Fragment() {
                     val train = childSnapshot.getValue(OwnTrainInfo::class.java)
                     train?.let {
                         binding.addedTrainName.text = it.trainName
+                        binding.addedTrainName.movementMethod = ScrollingMovementMethod()
                         binding.addedTrainDesc.text = it.trainDescription
+                        binding.addedTrainDesc.movementMethod = ScrollingMovementMethod()
                     }
                 }
             }
